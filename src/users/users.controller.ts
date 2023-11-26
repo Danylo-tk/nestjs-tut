e8183178e9a8b87acfc6327ca7bc46e8432ac2ac
Danylo Tkach <danylo.tkach@gmail.com>
2023-11-26 19:07:41 +0200

    nestjs-tut: injected users service methods to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,35 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UserType } from 'src/types/user';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   getAllUsers(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.getAllUsers(role);
   }
 
   @Get(':id')
   getSingleUser(@Param('id') id: string) {
-    return { id };
+    return this.usersService.getSingleUser(+id); // '+' (unary plus) operator converts id to a number
   }
 
   @Post()
-  createUser(@Body() user: unknown) {
-    return user;
+  createUser(@Body() user: UserType) {
+    return this.usersService.createUser(user);
   }
 
   @Patch(':id')
-  updateUser(@Param('id') id: string, @Body() userUpdate: { unknown }) {
-    return { id, ...userUpdate };
+  updateUser(@Param('id') id: string, @Body() userUpdate: Partial<UserType>) {
+    return this.usersService.updateUser(+id, userUpdate);
   }
 
   @Delete(':id')
   deleteUser(@Param('id') id: string) {
-    return { id };
+    return this.usersService.deleteUser(+id);
   }
 }
