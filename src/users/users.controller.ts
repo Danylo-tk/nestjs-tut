a762922e246b4e50048bf34c7acc2b78cda8ed55
Danylo Tkach <danylo.tkach@gmail.com>
2023-11-26 16:21:40 +0200

    nestjs-tut: added users cotroller skeleton

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getAllUsers(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  getSingleUser(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createUser(@Body() user: unknown) {
+    return user;
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() userUpdate: { unknown }) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return { id };
+  }
+}
